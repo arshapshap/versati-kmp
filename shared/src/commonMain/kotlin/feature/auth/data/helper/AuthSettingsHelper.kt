cbe639881a1a24f14d395e93c8f66693589668aa
arshapshap <arshapshap@mail.ru>
2024-06-02 13:37:33 +0300

    Move data and di of Auth feature to shared module

--- shared/src/commonMain/kotlin/feature/auth/data/helper/AuthSettingsHelper.kt
+++ shared/src/commonMain/kotlin/feature/auth/data/helper/AuthSettingsHelper.kt
@@ -3,6 +3,7 @@ package feature.auth.data.helper
 import com.russhwolf.settings.Settings
 
 private const val CURRENT_USER_KEY = "CURRENT_USER_KEY"
+
 class AuthSettingsHelper(
     private val settings: Settings
 ) {

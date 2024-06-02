f1ff5910d6af0bdf83d739108659d4ad34b75a9b
arshapshap <arshapshap@mail.ru>
2024-06-02 16:49:19 +0300

    Move data and di of Image Parsing feature to shared module

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -46,11 +46,7 @@ kotlin {
             implementation(libs.koin.core)
             implementation(libs.kotlinx.coroutines.core)
             implementation(libs.kotlinx.serialization.json)
-            implementation(libs.ktor.client.content.negotiation)
-            implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.okhttp)
-            implementation(libs.ktor.serialization.kotlinx.json)
-            implementation(libs.multiplatform.settings.no.arg)
             implementation(libs.orbit.core)
             implementation(projects.shared)
         }
